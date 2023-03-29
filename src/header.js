b5d8af712d047ce8e7943030b7e7bf5f0159a7a0
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2023-03-29 19:43:44 +0300

    Matille

---
+++ src/header.js
@@ -0,0 +1,12 @@
+import Omakuva from './omakuva.js';
+
+function Header() {
+  return(
+    <div class="p-5 oma-header text-white rounded">
+      <h1>Keskiviikon Portfolio</h1>
+      <p>Suvi Syrjäläinen</p>
+      <Omakuva/>
+    </div>);
+}
+
+export default Header;
---
+++ src/omakuva.js
@@ -0,0 +1,9 @@
+
+
+function Omakuva() {
+  return(
+  <img class="oma-kuva" src="https://avatars.githubusercontent.com/u/45297259?v=4"/>
+  );
+}
+
+export default Omakuva;

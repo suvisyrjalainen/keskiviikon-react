03ca6961869db9b1dd4a51d21b45d968f4e58afa
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2023-05-03 18:36:39 +0300

    router and navbar

--- src/index.js
+++ src/index.js
@@ -2,28 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Omakuva from './omakuva.js';
 import Header from './header.js';
-import Element from './element.js';
-import FluidGrid from 'react-fluid-grid'
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Portfolio from "./pages/Portfolio";
+import CV from "./pages/Cv";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
+
 import './styles.css'
 
 const myPage = <div>
 <Header/>
-<div class="fluid_container">
-<FluidGrid>
-<Element elementName="Unity 2d"
-image="https://cdn.pixabay.com/photo/2020/03/21/14/45/moon-4954228_960_720.jpg"
-imageAlt="Unity 2D esimerkkikuva"
-description="Tälläisen Unity 2D projektin olen tehnyt"
-link="https://github.com/suvisyrjalainen/Kodarit-2D-Unity-kaksintaistelu"
-/>
-<Element elementName="Unity 3d"
-image="https://kodarit.com/wp-content/uploads/2021/12/jalkapallopeli-750x350.png"
-imageAlt="Unity 2D esimerkkikuva"
-description="Tälläisen Unity #D projektin olen tehnyt"
-link="https://github.com/suvisyrjalainen/Kodarit-2D-Unity-kaksintaistelu"
-/>
-</FluidGrid>
-</div>
+<BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Portfolio />} />
+      <Route path="cv" element={<CV />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="*" element={<NoPage />} />
+    </Route>
+  </Routes>
+</BrowserRouter>
+
 </div>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
---
+++ src/pages/Cv.js
@@ -0,0 +1,5 @@
+const CV = () => {
+  return <h1>My CV</h1>;
+};
+
+export default CV;
